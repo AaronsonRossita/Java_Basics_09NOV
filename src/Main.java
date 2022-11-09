e5109c2d4972ccb54a038f671a1db13379af9c60
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-11-09 21:05:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // characters
+        String myString = "My first String";
+        char myChar = '#';
+
+        // numbers
+        byte myByte = 127;
+        short myShort = 32767;
+        int myInt = 2147483647;
+        long myLong = 9223372036854775807L;
+
+        double myDouble = 2.5;
+        float myFloat = 3.7f;
+
+        //boolean
+        boolean myTrueBoolean = true;
+        boolean myFalseBoolean = false;
+
+        int x = 4;
+        double y = 2.5;
+        y = x;
+
+        x = (int)2.7;
+
+        //System.out.println(x);
+
+        //System.out.println(myInt + myString);
+        //System.out.println("word" + 3 + 4);
+        //System.out.println(3 - 4 * 6 / 4 + 1 + "string" + 6 + 7);
+
+        String a = "abc";
+        char b = 'd';
+        b = a.charAt(1);
+        //System.out.println(b);
+
+        x += 10;
+        x *= 10;
+        x /= 10;
+        x -= 10;
+
+        //x % 10;
+
+
+    }
+
+}
